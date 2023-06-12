48e5a40e77871f3305030bb301a32b5b2a817993
Aziz <19191@.ru>
2023-06-12 13:35:23 +0500

    save

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let name = prompt('Введите ваше имя');
+let year = +prompt('Введите сегоднешный год');
+let bornYear = +prompt('Введите вашe год рождение');
+
+
+let res = function() {
+    let result = (year - bornYear)
+    return result
+}
+
+console.log(name + ', Ваш возраст ' + res(
